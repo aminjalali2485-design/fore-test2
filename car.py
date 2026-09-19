be798764dc848e04a64af1d5a4ffefe27bf9b8b7
Amin <aminjalali2485@gmail.com'git config --global user.email aminjalali2485@gmail.com>
2026-09-19 13:16:17 +0330

    the Second commit

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 
 
 class Car:
-    def __init__(self,make,model,year):
+    def __init__(self,make,model,year,color):
         self.make = make
         self.model = model
         self.year = year
+        self.color = color
         self.odometer_reading = 0
     def get_descriptive_name(self):
         long_name = f'{self.year} {self.make} {self.model}'
