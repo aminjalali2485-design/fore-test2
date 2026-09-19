d74803d1a1c92636f14e02f3c1fe5f8c79f9eef9
aminjalali <config>
2026-09-19 13:26:09 +0330

    the forth commit

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 
 
 class Car:
-    def __init__(self,make,model,year):
+    def __init__(self,make,model,year,cool):
         self.make = make
         self.model = model
         self.year = year
+        self.cool = cool
         self.odometer_reading = 0
     def get_descriptive_name(self):
         long_name = f'{self.year} {self.make} {self.model}'
